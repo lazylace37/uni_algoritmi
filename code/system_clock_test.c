396762601aa390211e948a683fa4177ad98bfde6
Gioele Vuaran <vuarangioele@gmail.com>
2025-03-02 15:22:43 +0100

    feat: add system_clock_test exec

---
+++ code/system_clock_test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+
+static inline double duration(struct timespec start, struct timespec end) {
+  return end.tv_sec - start.tv_sec +
+         ((end.tv_nsec - start.tv_nsec) / (double)1000000000.0);
+}
+
+double getResolution() {
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  do {
+    clock_gettime(CLOCK_MONOTONIC, &end);
+  } while (duration(start, end) == 0.0);
+  return duration(start, end);
+}
+
+int main() {
+  double resolution = getResolution();
+  printf("System clock resolution: %.15f\n", resolution);
+  return 0;
+}
