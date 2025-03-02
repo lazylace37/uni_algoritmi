1a8a4dc1af98aaf3aad30aea3d98a73847e56471
Gioele Vuaran <vuarangioele@gmail.com>
2025-03-02 15:48:34 +0100

    refactor(counting_sort): range is [0,k)

--- code/counting_sort.c
+++ code/counting_sort.c
@@ -4,15 +4,15 @@
 
 void counting_sort(void *arr, size_t n, size_t k, size_t el_size, void *out,
                    get_key get_key, allocator_t allocator) {
-  int *c = allocator.alloc((k + 1) * sizeof(int), allocator.state);
-  for (size_t j = 0; j < k + 1; j++) { // Θ(k)
+  int *c = allocator.alloc(k * sizeof(int), allocator.state);
+  for (size_t j = 0; j < k; j++) { // Θ(k)
     c[j] = 0;
   }
   for (size_t i = 0; i < n; i++) { // Θ(n)
     int key = get_key(arr + i * el_size);
     c[key]++;
   }
-  for (size_t j = 1; j < k + 1; j++) { // Θ(k)
+  for (size_t j = 1; j < k; j++) { // Θ(k)
     c[j] += c[j - 1];
   }
   for (ssize_t i = n - 1; i >= 0; i--) { // Θ(n)
