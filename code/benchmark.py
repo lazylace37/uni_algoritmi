1a2c53d0993f4cb71dce3d4ceaa39fd3515fb3c1
Gioele Vuaran <16881832+lazylace37@users.noreply.github.com>
2025-04-26 15:22:43 +0200

    refactor: benchmark.py

--- code/benchmark.py
+++ code/benchmark.py
@@ -2,24 +2,37 @@ import multiprocessing
 import subprocess
 from functools import partial
 from multiprocessing import Manager, Pool
+from multiprocessing.managers import DictProxy
+from typing import List, Tuple
 
 import matplotlib
-
-matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+
+matplotlib.use("pgf")
+matplotlib.rcParams.update(
+    {
+        "pgf.texsystem": "pdflatex",
+        "font.family": "serif",
+        "text.usetex": True,
+        "pgf.rcfonts": False,
+    }
+)
 
 N_PROC = multiprocessing.cpu_count() // 2
 N_RUNS = 20
 
+BenchmarkRun = Tuple[List[float], List[float]]
+Benchmark = Tuple[int, BenchmarkRun]
 
-def execute_bench(n: int, m: int):
+
+def execute_bench(n: int, m: int) -> BenchmarkRun:
     result = subprocess.run(
         ["./bench", str(n), str(m), str(N_RUNS)], capture_output=True, text=True
     )
 
     if result.returncode != 0:
         print(f"Error for {n}: {result.stderr.strip()}")
-        return
+        return [], []
 
     output = result.stdout.strip().split("\n")
     exec_times = [float(x) for x in output[1].split("\t")]
@@ -27,25 +40,64 @@ def execute_bench(n: int, m: int):
     return exec_times, exec_stddev
 
 
-def run_experiment(n: int, m: int, res: dict):
+def run_experiment(n: int, m: int, res: "DictProxy[int, BenchmarkRun]"):
     res[n] = execute_bench(n, m)
 
 
-def run_experiment_l(m: int, n: int, res: dict):
+def run_experiment_l(m: int, n: int, res: "DictProxy[int, BenchmarkRun]"):
     res[m] = execute_bench(n, m)
 
 
-def setup_chart(times_map):
-    x = [n for n, _ in times_map]
-    mean_times = [run[0] for _, run in times_map]
-    std_devs = [run[1] for _, run in times_map]
-    quick_sort, counting_sort, quick_sort_3_way, heap_sort = list(zip(*mean_times))
+def setup_chart_data(times: List[Benchmark]):
+    xs: List[int] = []
+    quick_sort_means: List[float] = []
+    counting_sort_means: List[float] = []
+    quick_sort_3_way_means: List[float] = []
+    heap_sort_means: List[float] = []
+
+    quick_sort_stddevs: List[float] = []
+    counting_sort_stddevs: List[float] = []
+    quick_sort_3_way_stddevs: List[float] = []
+    heap_sort_stddevs: List[float] = []
+
+    for n, (means, stddevs) in times:
+        xs.append(n)
+
+        quick_sort_means.append(means[0])
+        counting_sort_means.append(means[1])
+        quick_sort_3_way_means.append(means[2])
+        heap_sort_means.append(means[3])
+
+        quick_sort_stddevs.append(stddevs[0])
+        counting_sort_stddevs.append(stddevs[1])
+        quick_sort_3_way_stddevs.append(stddevs[2])
+        heap_sort_stddevs.append(stddevs[3])
+
+    return (
+        xs,
+        quick_sort_means,
+        counting_sort_means,
+        quick_sort_3_way_means,
+        heap_sort_means,
+        quick_sort_stddevs,
+        counting_sort_stddevs,
+        quick_sort_3_way_stddevs,
+        heap_sort_stddevs,
+    )
+
+
+def setup_chart(times: list[tuple[int, tuple[list[float], list[float]]]]):
     (
+        x,
+        quick_sort,
+        counting_sort,
+        quick_sort_3_way,
+        heap_sort,
         quick_sort_stddev,
         counting_sort_stddev,
         quick_sort_3_way_stddev,
         heap_sort_stddev,
-    ) = list(zip(*std_devs))
+    ) = setup_chart_data(times)
 
     plt.figure(figsize=(12, 8))
     plt.plot(x, quick_sort, marker="s", label="Quick Sort")
@@ -82,46 +134,52 @@ def setup_chart(times_map):
 def main():
     with Manager() as manager:
         ## Grafico in funzione di n
-        n = [int(100 * 1.072267222**i) for i in range(0, 100)]
+        m = 100_000
+        # for m in range(100_000, 1_000_000, 100_000):
+        if True:
+            n = [int(100 * 1.072267222**i) for i in range(0, 100)]
 
-        res_map = manager.dict()
-        with Pool(processes=N_PROC) as pool:
-            pool.map(partial(run_experiment, m=100_000, res=res_map), n)
-        times_map = sorted(res_map.items())
+            res_map = manager.dict()
+            with Pool(processes=N_PROC) as pool:
+                pool.map(partial(run_experiment, m=m, res=res_map), n)
+            times = sorted(res_map.items())
 
-        plt = setup_chart(times_map)
-        plt.suptitle("Sorting Algorithm Performance Comparison")
-        plt.title("m = 100,000")
-        plt.xlabel("Input Size (n)")
-        plt.ylabel("Execution Time (seconds)")
-        plt.legend()
-        plt.grid(True, which="both", linestyle="--")
-        plt.savefig("benchmark_n_m=100_000.png")
+            plt = setup_chart(times)
+            plt.suptitle("Sorting Algorithm Performance Comparison")
+            plt.title(f"m = {m}")
+            plt.xlabel("Input Size (n)")
+            plt.ylabel("Execution Time (seconds)")
+            plt.legend()
+            plt.grid(True, which="both", linestyle="--")
+            plt.savefig(f"output/benchmark_n_m={m}.pgf")
 
-        plt.xscale("log")
-        plt.yscale("log")
-        plt.savefig("benchmark_n_m=100_000_log.png")
+            plt.xscale("log")
+            plt.yscale("log")
+            plt.savefig(f"output/benchmark_n_m={m}_log.pgf")
 
         ## Grafico in funzione di m
-        m = [int(10 * 1.123324033**i) for i in range(0, 100)]
+        # for n in range(10_000, 100_000, 10_000):
+        n = 10_000
+        if True:
+            m = [int(10 * 1.123324033**i) for i in range(0, 100)]
 
-        res_map_1 = manager.dict()
-        with Pool(processes=N_PROC) as pool:
-            pool.map(partial(run_experiment_l, n=10_000, res=res_map_1), m)
-        times_map = sorted(res_map_1.items())
+            res_map_1 = manager.dict()
+            with Pool(processes=N_PROC) as pool:
+                pool.map(partial(run_experiment_l, n=n, res=res_map_1), m)
+            times = sorted(res_map_1.items())
 
-        plt = setup_chart(times_map)
-        plt.suptitle("Sorting Algorithm Performance Comparison")
-        plt.title("n = 10,000")
-        plt.xlabel("Range (m)")
-        plt.ylabel("Execution Time (seconds)")
-        plt.legend()
-        plt.grid(True, which="both", linestyle="--")
-        plt.savefig("benchmark_m_n=10_000.png")
+            plt = setup_chart(times)
+            plt.suptitle("Sorting Algorithm Performance Comparison")
+            plt.title(f"n = {n}")
+            plt.xlabel("Range (m)")
+            plt.ylabel("Execution Time (seconds)")
+            plt.legend()
+            plt.grid(True, which="both", linestyle="--")
+            plt.savefig(f"output/benchmark_m_n={n}.pgf")
 
-        plt.xscale("log")
-        plt.yscale("log")
-        plt.savefig("benchmark_m_n=10_000_log.png")
+            plt.xscale("log")
+            plt.yscale("log")
+            plt.savefig(f"output/benchmark_m_n={n}_log.pgf")
 
 
 if __name__ == "__main__":
